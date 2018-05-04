bba61f229be1873d18f493bea84f864f4c3bc43f
dyuan <dennis.yuan@terrapages.com>
2018-05-04 16:02:18 +1000

    http://learnyousomeerlang.com/recursion#more-than-lists

---
+++ src/quick_sort.erl
@@ -0,0 +1,15 @@
+-module(quick_sort).
+-export([]).
+-compile([export_all]).
+
+
+quicksort([]) -> [];
+quicksort([Pivot|Rest]) ->
+  {Smaller, Larger} = partition(Pivot,Rest,[],[]),
+  quicksort(Smaller) ++ [Pivot] ++ quicksort(Larger).
+
+partition(_,[],Smaller,Larger) -> {Smaller,Larger};
+partition(Pivot,[H|T],Smaller,Larger) ->
+  if H =< Pivot -> partition(Pivot, T, [H|Smaller], Larger);
+     H >= Pivot -> partition(Pivot, T, Smaller, [H|Larger])
+  end.
