619b743ef10aadfdfb497f949841a2be13f0d90f
dyuan <dennis.yuan@terrapages.com>
2018-05-04 19:41:51 +1000

    7:41PM 4/05/2018

--- src/quick_sort.erl
+++ src/quick_sort.erl
@@ -3,13 +3,13 @@
 -compile([export_all]).
 
 
-quicksort([]) -> [];
-quicksort([Pivot|Rest]) ->
+qsort([]) -> [];
+qsort([Pivot|Rest]) ->
   {Smaller, Larger} = partition(Pivot,Rest,[],[]),
-  quicksort(Smaller) ++ [Pivot] ++ quicksort(Larger).
+  qsort(Smaller) ++ [Pivot] ++ qsort(Larger).
 
 partition(_,[],Smaller,Larger) -> {Smaller,Larger};
 partition(Pivot,[H|T],Smaller,Larger) ->
   if H =< Pivot -> partition(Pivot, T, [H|Smaller], Larger);
-     H >= Pivot -> partition(Pivot, T, Smaller, [H|Larger])
+     H > Pivot -> partition(Pivot, T, Smaller, [H|Larger])
   end.
