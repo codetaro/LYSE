cb3aabc97a064e5f4c801947193d7b538fb75ede
dyuan <dennis.yuan@terrapages.com>
2018-06-08 17:46:25 +1000

    http://learnyousomeerlang.com/the-count-of-applications

---
+++ ebin/ppool.app
@@ -0,0 +1,13 @@
+
+{application, ppool, [
+%%  {description, ""},
+  {vsn, "1.0.0"},
+  {modules, [ppool, ppool_serv, ppool_sup, ppool_supersup, ppool_worker_sup]},
+  {registered, [ppool]},
+%%  {applications, [
+%%    kernel,
+%%    stdlib
+%%  ]},
+  {mod, {ppool, []}}
+%%  {env, []}
+]}.
--- src/ppool_supersup.erl
+++ src/ppool_supersup.erl
@@ -4,7 +4,7 @@
 -behaviour(supervisor).
 
 %% API
--export([start_link/0, stop/0, start_pool/3, stop_pool/1]).
+-export([start_link/0, start_pool/3, stop_pool/1]).
 
 %% Supervisor callbacks
 -export([init/1]).
@@ -18,12 +18,12 @@ start_link() ->
   supervisor:start_link({local, ppool}, ?MODULE, []).
 
 %% stop the whole application
-stop() ->
-  case whereis(ppool) of
-    P when is_pid(P) ->
-      exit(P, kill);
-    _ -> ok
-  end.
+%%stop() ->
+%%  case whereis(ppool) of
+%%    P when is_pid(P) ->
+%%      exit(P, kill);
+%%    _ -> ok
+%%  end.
 
 %% create a specific pool
 start_pool(Name, Limit, MFA) ->
